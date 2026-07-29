04ff3375fbd10b4c887265b3a7190b76d15fa1e7
Jose Garcia Vargas <j.garcia05@live.com>
2026-07-29 18:35:54 -0400

    next_date2

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -519,7 +519,7 @@ class CommonController
 
         // Buscar si ya existe un registro del doctor creado hoy
         $medicalAssistance = MedicalAssistance::where('doctor_id', $validated['doctorId'])
-            ->whereDate('created_at', Carbon::today())
+            ->whereDate('next_date', Carbon::today())
             ->first();
 
         if ($medicalAssistance) {
